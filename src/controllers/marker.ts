35911cf2cf029cb3f10d8ae2f8d270129fe78f4e
lnkkerst <1593510417@qq.com>
2022-07-08 10:45:51 +0800

    feat: CRUD

--- src/controllers/marker.ts
+++ src/controllers/marker.ts
@@ -1,25 +1,71 @@
 import { Context } from "koa";
 import MarkerModel from "../models/Marker";
+import { v4 as uuidv4 } from "uuid";
 
 export default {
   getMarker: async (ctx: Context, next: any) => {
+    try {
+      ctx.response.body = {
+        result: "succeed",
+        data: MarkerModel.findAll({
+          where: {
+            id: ctx.request.query.id,
+          },
+        }),
+      };
+    } catch (_e) {
+      ctx.throw(400);
+    }
     await next();
   },
   postMarker: async (ctx: Context, next: any) => {
-    await next();
-  },
-  deleteMarker: async (ctx: Context, next: any) => {
-    ctx.response.body = {
-      result: await MarkerModel.destroy({
+    try {
+      await MarkerModel.update(ctx.request.body.data, {
         where: {
           id: ctx.request.body.id,
         },
-      }) == 1,
-    };
+      });
+      ctx.response.body = {
+        result: "succeed",
+      };
+    } catch (_e) {
+      const e = _e as Error;
+      console.log(e.message);
+      ctx.throw(400);
+    }
+    await next();
+  },
+  deleteMarker: async (ctx: Context, next: any) => {
+    try {
+      ctx.response.body = {
+        result:
+          (await MarkerModel.destroy({
+            where: {
+              id: ctx.request.body.id,
+            },
+          })) == 1
+            ? "succeed"
+            : "failed",
+      };
+    } catch (_e) {
+      const e = _e as Error;
+      console.log(e.message);
+      ctx.throw(400);
+    }
     await next();
   },
   putMarker: async (ctx: Context, next: any) => {
-    await MarkerModel.create(ctx.request.body);
+    try {
+      ctx.request.body.id = uuidv4();
+      await MarkerModel.create(ctx.request.body);
+      ctx.response.body = {
+        result: "succeed"
+      }
+    } catch (_e) {
+      const e = _e as Error;
+      console.log(e.message);
+      ctx.throw(400);
+    }
     await next();
   },
 };
--- src/models/Marker.ts
+++ src/models/Marker.ts
@@ -8,27 +8,35 @@ const MarkerModel = sequelize.define<MarkerInstance>("Marker", {
   id: {
     primaryKey: true,
     type: DataTypes.STRING,
+    allowNull: false,
   },
   name: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
   englishName: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
   type: {
     type: DataTypes.NUMBER,
+    allowNull: false,
   },
   position: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
   belongTo: {
     type: DataTypes.NUMBER,
+    allowNull: false,
   },
   openTime: {
     type: DataTypes.STRING,
+    allowNull: false,
   },
   extraInfo: {
     type: DataTypes.CHAR(10240),
+    allowNull: false,
   },
 });
 
